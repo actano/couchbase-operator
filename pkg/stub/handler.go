6da05f494cd8c5cbba12ab87c0ca910202c7027e
Daniel Paschke <paschdan@gmail.com>
2018-06-12 16:16:05 +0200

    feat: add nodeInitialization with hostname

--- pkg/stub/handler.go
+++ pkg/stub/handler.go
@@ -26,8 +26,15 @@ type Handler struct {
 func (h *Handler) Handle(ctx context.Context, event sdk.Event) error {
 	switch o := event.Object.(type) {
 	case *v1alpha1.Couchbase:
-		pod := newCouchbasePod(o)
-		err := sdk.Create(pod)
+		service := newCouchbaseService(o)
+		err := sdk.Create(service)
+		if err != nil && !errors.IsAlreadyExists(err) {
+			logrus.Errorf("Failed to create couchbase service : %v", err)
+			return err
+		}
+
+		pod := newCouchbasePod(o, "001")
+		err = sdk.Create(pod)
 		if err != nil && !errors.IsAlreadyExists(err) {
 			logrus.Errorf("Failed to create couchbase pod : %v", err)
 			return err
@@ -50,13 +57,21 @@ func (h *Handler) Handle(ctx context.Context, event sdk.Event) error {
 		logrus.Infof("Couchbase PodIp: %s", podIp)
 
 
-
 		couchbaseClient := couchbase.New("admin", "password")
 		couchbaseClient.SetEndpoints([]string{"http://" + podIp + ":8091"})
 
 
 		_, err = couchbaseClient.ClusterInfo()
 		if err != nil {
+
+			// initialize node...
+			hostname := "couchbase001.couchbase." + o.Namespace + ".svc"
+			datapath := "/opt/couchbase/var/lib/couchbase/data"
+			err = couchbaseClient.NodeInitialize(hostname, datapath, datapath, []string{})
+			if err != nil {
+				logrus.Errorf("failed to initialize node: %v", err)
+				return err
+			}
 
 			err := initalizeCluster(couchbaseClient)
 			if err != nil {
@@ -73,8 +88,32 @@ func initalizeCluster(couchbaseClient *couchbase.Couchbase) error {
 	return couchbaseClient.ClusterInitialize("admin", "password", &poolDefaults, 8091, []couchbase.ServiceName{couchbase.DataService}, "" )
 }
 
+func newCouchbaseService(cr *v1alpha1.Couchbase) *corev1.Service {
+	return &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			Kind: "Service",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: cr.Namespace,
+			Name:      "couchbase",
+
+		},
+		Spec: corev1.ServiceSpec{
+			ClusterIP: "None",
+			Ports: []corev1.ServicePort{
+				{
+					Name: "http",
+					Port: 8091,
+				},
+			},
+			Selector: labelsForCouchbase(cr.Name),
+		},
+	}
+}
+
 // newbusyBoxPod demonstrates how to create a busybox pod
-func newCouchbasePod(cr *v1alpha1.Couchbase) *corev1.Pod {
+func newCouchbasePod(cr *v1alpha1.Couchbase, id string) *corev1.Pod {
 	labels := labelsForCouchbase(cr.Name)
 	return &corev1.Pod{
 		TypeMeta: metav1.TypeMeta{
@@ -82,7 +121,7 @@ func newCouchbasePod(cr *v1alpha1.Couchbase) *corev1.Pod {
 			APIVersion: "v1",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "couchbase",
+			Name:      "couchbase" + id,
 			Namespace: cr.Namespace,
 			OwnerReferences: []metav1.OwnerReference{
 				*metav1.NewControllerRef(cr, schema.GroupVersionKind{
@@ -94,6 +133,8 @@ func newCouchbasePod(cr *v1alpha1.Couchbase) *corev1.Pod {
 			Labels: labels,
 		},
 		Spec: corev1.PodSpec{
+			Hostname: "couchbase" + id,
+			Subdomain: "couchbase",
 			Containers: []corev1.Container{
 				{
 					Name:    "couchbase",
